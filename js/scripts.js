8eea8c1e01663567f3e8d5c119251320539e6c26
Allison Varady <avarady@eagles.ewu.edu>
2019-02-02 13:26:07 -0800

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+// Everything OK?
+console.log('OK');
+
+/* Lettering JS Plugin
+* Reference: https://github.com/davatron5000/Lettering.js/wiki
+*/
+
+// default
+// $('h1').lettering();
+
+// with Words options
+// $('h1').lettering('words');
+
+// with Lines options
+// $('h1').lettering('lines');
+
+/* Media Element Player Plugin
+* Reference: https://github.com/mediaelement/mediaelement/blob/master/docs/usage.md
+*/
+
+$('video').mediaelementplayer();
